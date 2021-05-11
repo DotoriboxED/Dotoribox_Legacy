e25f5e30f05154afd975208717ef4c7a8b73f1c4
Song <songyw9812@gmail.com>
2021-05-11 14:42:26 +0900

    오타 수정

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -6,8 +6,8 @@ const api = axios.create({
 });
 
 export const SampleApi = {
-    getList: () => api.post("http://localhost:5000/api/sample"),//샘플 리스트 가져오기 page2
-    getInfo: (body, id) => api.post("http://localhost:5000/api/sample/" + id, body),//선택된 샘플 정보 가져오기
+    getList: () => api.get("http://localhost:5000/api/sample"),//샘플 리스트 가져오기 page2
+    getInfo: (body, id) => api.get("http://localhost:5000/api/sample/" + id, body),//선택된 샘플 정보 가져오기
     getInfoImage: (id) => api.get("http://localhost:5000/api/sample/" + id + "/image"),
     postInfo: (body, id) => api.post("http://localhost:5000/api/review/" + id, body)//택시번호, 샘플, 유저정보 디비에 저장
 }
