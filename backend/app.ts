61ab285b5178f72c8a5fe057fc45a16cde041beb
Song <songyw9812@gmail.com>
2021-05-14 13:26:54 +0900

    fix cors

--- backend/app.ts
+++ backend/app.ts
@@ -5,6 +5,7 @@ import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 import indexRouter from './router/index';
 import helmet from 'helmet';
+import cors from 'cors';
 
 import apiRouter from './router/api';
 const app = express();
@@ -19,6 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(process.cwd(), 'public')));
+app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/api', apiRouter);
